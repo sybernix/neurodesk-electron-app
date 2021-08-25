be173c3720e3395576d74161f52dc6f9d69dfb1c
niruhan <niruhanv@gmail.com>
2021-08-25 20:05:21 +0530

    Add button to open vnm in browser

--- renderer.js
+++ renderer.js
@@ -5,17 +5,26 @@
 // selectively enable features needed in the rendering
 // process.
 const exec = require('child_process').exec
+const shell = require('electron').shell;
 
 function runVnmFirst() {
     exec('sudo docker run --privileged --name vnm -v ~/vnm:/vnm -v /dev/shm:/dev/shm -e USER=neuro -p 6080:80 vnmd/vnm:20210523', (err, stdout, stderr) => {
-        console.log(stdout)
+        console.log(err);
+        console.log(stdout);
+        console.log(stderr)
     });
 }
 
 function startVnm() {
     exec('sudo docker start vnm', (err, stdout, stderr) => {
-        console.log(stdout)
+        console.log(err);
+        console.log(stdout);
+        console.log(stderr)
     });
+}
+
+function openVnm() {
+    shell.openExternal('http://localhost:6080');
 }
 
 document.querySelector('#runVnmFirst').addEventListener('click', () => {
@@ -24,4 +33,8 @@ document.querySelector('#runVnmFirst').addEventListener('click', () => {
 
 document.querySelector('#startVnm').addEventListener('click', () => {
     startVnm()
+})
+
+document.querySelector('#openVnm').addEventListener('click', () => {
+    openVnm()
 })
